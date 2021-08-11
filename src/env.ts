d394dc95aa9864323674f94f3f21da824f0f2cf0
SkyeYoung <iskyex@outlook.com>
2021-08-11 21:29:06 +0800

    refactor(env.ts): stop process immediately

--- src/env.ts
+++ src/env.ts
@@ -3,5 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 if (!process.env.GITHUB_ACCESS_TOKEN) {
-  throw new Error('has no GITHUB_ACCESS_TOKEN');
+  console.error('has no GITHUB_ACCESS_TOKEN');
+  process.exit(1);
 }
